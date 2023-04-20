f001243e8229834d250e63048ac4b22119ab2fd6
SShidou <79430377+SShidou@users.noreply.github.com>
2023-04-20 10:25:31 +0700

    Restlying profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { faPencil, faWrench } from '@fortawesome/free-solid-svg-icons';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  faEdit = faPencil;
+  faRef = faWrench;
 
+  constructor(private cs: CartService, private router: Router) {
+    // this.cs.getCartObservable().subscribe((cart) => {
+    //   this.cart = cart;
+    // });
+  }
 }
