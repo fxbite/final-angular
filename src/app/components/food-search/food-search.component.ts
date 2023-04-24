b1d0b629e412626d6edbc782ebea133fb22462ba
SShidou <79430377+SShidou@users.noreply.github.com>
2023-04-24 15:11:07 +0700

    Restyle

--- src/app/components/food-search/food-search.component.ts
+++ src/app/components/food-search/food-search.component.ts
@@ -4,34 +4,34 @@ import { FoodService } from '../../services/food.service';
 import { IFood } from '../../shared/interfaces/IFood';
 
 @Component({
-  selector: 'app-food-search',
-  templateUrl: './food-search.component.html',
-  styleUrls: ['./food-search.component.scss']
+ selector: 'app-food-search',
+ templateUrl: './food-search.component.html',
+ styleUrls: ['./food-search.component.scss']
 })
 export class FoodSearchComponent {
-  searchTerm!: string;
-  tags?: IFood[];
-  value2!: string;
+ searchTerm!: string;
+ tags?: IFood[];
+ value2!: string;
 
-  @ViewChild('searchFood', { static: false }) searchFood!: ElementRef;
+ @ViewChild('searchFood', { static: false }) searchFood!: ElementRef;
 
-  // @Output
+ // @Output
 
-  constructor(private ac: ActivatedRoute, private router: Router, private foodService: FoodService) {
-    this.ac.params.subscribe((params) => {
-      if (params['searchTerm']) this.searchTerm = params['searchTerm'];
-    });
+ constructor(private ac: ActivatedRoute, private router: Router, private foodService: FoodService) {
+  this.ac.params.subscribe((params) => {
+   if (params['searchTerm']) this.searchTerm = params['searchTerm'];
+  });
 
-    this.foodService.getAllTags().subscribe((data) => {
-      this.tags = data;
-    });
-  }
+  this.foodService.getAllTags().subscribe((data) => {
+   this.tags = data;
+  });
+ }
 
-  // search(event: any) {
-  //   let term = event.target.getAttribute('value');
-  search(term: string): void {
-    if (term) this.router.navigateByUrl('/menu?search=' + term);
-    const inputValue = this.searchFood.nativeElement.value;
-    console.log('Input value:', inputValue);
-  }
+ // search(event: any) {
+ //   let term = event.target.getAttribute('value');
+ search(term: string): void {
+  if (term) this.router.navigateByUrl('/menu?search=' + term);
+  const inputValue = this.searchFood.nativeElement.value;
+  console.log('Input value:', inputValue);
+ }
 }
